bceaa7a670a724e6db4a3f2d18badb9b6602474f
NguetchuissiBrunel <landrybrunel5@gmail.com>
2026-02-05 12:54:36 +0100

    ajout type message

---
+++ src/types/message.ts
@@ -0,0 +1,8 @@
+export interface Message {
+    content: { stringValue: string };
+    created_at: { timestampValue: string };
+    destinataire: { stringValue: string };
+    genre: { stringValue: string };
+    likes: { integerValue: string };
+    pseudo: { stringValue: string };
+}
