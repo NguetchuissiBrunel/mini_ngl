6fa3336d9d8c5c4710a72979af11ad148a432333
PIO-VIA <piodjiele@gmail.com>
2026-02-06 14:06:36 +0100

    feat: add Google Analytics component and integrate it into layout.tsx with conditional rendering.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import './globals.css'
 import ThemeInitializer from '@/components/ThemeInitializer'
 import { ModalProvider } from '@/context/ModalContext'
 import CustomModal from '@/components/CustomModal'
+import GoogleAnalytics from '@/components/GoogleAnalytics'
+
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 const allura = Allura({ weight: '400', subsets: ['latin'], variable: '--font-allura' })
@@ -24,7 +26,11 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body className={`${inter.variable} ${allura.variable} font-sans antialiased`}>
+        {process.env.NEXT_PUBLIC_GA_ID && (
+          <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GA_ID} />
+        )}
         <ThemeInitializer />
+
         <ModalProvider>
           <main>
             {children}
